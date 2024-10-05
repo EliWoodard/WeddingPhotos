fdaff083f10ae61974d35834b7d65d06729ab8e4
EliWoodard <eliwoodard@u.boisestate.edu>
2024-10-05 12:13:50 -0600

    Added index.html

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 // next.config.js
+/** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
-};  
+    output: 'export',
+    images: {
+      unoptimized: true, // Required for using Next.js images with static exports
+    },
+    basePath: '/YOUR_GITHUB_REPO_NAME', // Change to your GitHub repo name
+  };
