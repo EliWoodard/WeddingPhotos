ae03f89680880d27399a66acb94d733485dd9a4c
EliWoodard <eliwoodard@u.boisestate.edu>
2024-10-08 12:22:31 -0600

    Updates

---
+++ _next/static/MfODDpkhc7579auIYOFLs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-8c66d5c2a5389b4d.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
